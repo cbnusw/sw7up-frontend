2b23bbf2d377c1e4f6da03a040986f3768424da9
kmahn <ahnmo777@gmail.com>
2022-02-10 10:55:17 +0900

    update

--- projects/main/src/app/pages/education-pages/basic-page/basic-page.component.ts
+++ projects/main/src/app/pages/education-pages/basic-page/basic-page.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+const TAB_MENUS = [
+  'SW기초교육',
+  'SW체험교육',
+];
+declare type TTabMenu = typeof TAB_MENUS[number];
+
 @Component({
   selector: 'sw-basic-page',
   templateUrl: './basic-page.component.html',
@@ -7,7 +13,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicPageComponent implements OnInit {
 
-  constructor() { }
+  selectedTab: TTabMenu;
+  tabs: TTabMenu[] = TAB_MENUS;
+
+  constructor() {
+    this.selectedTab = this.tabs[0];
+  }
+
+  changeType(tab: string): boolean {
+    this.selectedTab = tab;
+    return false;
+  }
 
   ngOnInit(): void {
   }
--- projects/main/src/app/pages/education-pages/online-page/online-page.component.ts
+++ projects/main/src/app/pages/education-pages/online-page/online-page.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+interface IOnlineLecture {
+  name: string;
+  target?: string;
+  link: string;
+}
+
 @Component({
   selector: 'sw-online-page',
   templateUrl: './online-page.component.html',
@@ -7,7 +13,102 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OnlinePageComponent implements OnInit {
 
-  constructor() { }
+  moocLectures: Array<IOnlineLecture> = [
+    {
+      name: '아두이노 프로그래밍',
+      link: 'https://www.edwith.org/arduinoprogram',
+    },
+    {
+      name: '정보기술의 이해',
+      link: 'https://www.edwith.org/info01',
+    },
+    {
+      name: '컴퓨팅 사고력 기르기',
+      link: 'https://www.edwith.org/comthink',
+    },
+    {
+      name: '스마트홈 구축을 위한 아두이노 센서 활용',
+      link: 'https://www.edwith.org/iotsensor',
+    },
+    {
+      name: '파이썬 프로그래밍',
+      link: 'https://www.edwith.org/python5',
+    },
+    {
+      name: '인공지능',
+      link: 'https://www.edwith.org/ailecture',
+    },
+    {
+      name: '빅데이터분석 시각화',
+      link: 'https://www.edwith.org/bigdata1',
+    },
+  ];
+
+  kocwLectures: Array<IOnlineLecture> = [
+    {
+      name: '기초컴퓨터프로그래밍(충북대학교 안재형)',
+      target: '청소년/일반인/초・중등교사',
+      link: 'http://www.kocw.net/home/search/kemView.do?kemId=1327393'
+    },
+    {
+      name: '기초컴퓨터프로그래밍(충북대학교 이건명)',
+      target: '청소년/일반인/초・중등교사',
+      link: 'http://www.kocw.net/home/search/kemView.do?kemId=438105'
+    },
+    {
+      name: '모바일프로그래밍실습',
+      target: '일반인/대학생',
+      link: 'http://www.kocw.net/home/search/kemView.do?kemId=1188087'
+    },
+    {
+      name: '선형대수학',
+      target: '대학생',
+      link: 'http://www.kocw.net/home/search/kemView.do?kemId=908046'
+    },
+    {
+      name: '웹기반 소프트웨어개발',
+      target: '일반인/대학생',
+      link: 'http://www.kocw.net/home/search/kemView.do?kemId=1116118'
+    },
+    {
+      name: '응용컴퓨터프로그래밍',
+      target: '청소년/일반인/대학생',
+      link: 'http://www.kocw.net/home/search/kemView.do?kemId=1344156'
+    },
+    {
+      name: '인간 상호작용 프로그램',
+      target: '대학생',
+      link: 'http://www.kocw.net/home/search/kemView.do?kemId=849733'
+    },
+    {
+      name: '인공지능',
+      target: '일반인/재직자',
+      link: 'http://www.kocw.net/home/search/kemView.do?kemId=1170523'
+    },
+    {
+      name: '자료구조',
+      target: '대학생',
+      link: 'http://www.kocw.net/home/search/kemView.do?kemId=446623'
+    },
+    {
+      name: '자바프로그래밍',
+      target: '일반인/대학생/초・중등교사',
+      link: 'http://www.kocw.net/home/search/kemView.do?kemId=330713'
+    },
+    {
+      name: '자바네트워크프로그래밍',
+      target: '일반인',
+      link: 'http://www.kocw.net/home/cview.do?cid=f126689618b77cf1'
+    },
+    {
+      name: '컴퓨터그래픽스',
+      target: '대학생',
+      link: 'http://www.kocw.net/home/search/kemView.do?kemId=335239'
+    },
+  ];
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
