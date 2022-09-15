babb177b35d6a905f40ed2194fd68617dff9c03a
kmahn <ahnmo777@gmail.com>
2022-09-15 13:22:06 +0900

    로그인 후 리다이렉트 URL 오류 수정

--- projects/project/src/environments/environment.prod.ts
+++ projects/project/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ import { environment as env } from '../../../../global/environments/environment.
 
 export const environment = {
   ...env,
-  host: 'https://sw7up.cbnu.ac.kr/project/',
+  host: 'https://sw7up.cbnu.ac.kr/project',
   loginPageUrl: 'https://sw7up.cbnu.ac.kr/account/login'
 };
