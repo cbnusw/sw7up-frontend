7ac7a24d5611e95520eadee08550e660e311445c
kmahn <ahnmo777@gmail.com>
2023-05-26 19:08:57 +0900

    Update

--- projects/project/src/app/pages/professor-pages/components/projects-chart/projects-chart.component.ts
+++ projects/project/src/app/pages/professor-pages/components/projects-chart/projects-chart.component.ts
@@ -130,10 +130,11 @@ export class ProjectsChartComponent implements OnInit, AfterViewInit {
     if (!this.chart) {
       return;
     }
-
-    this.chart.data.labels = this.labels.map(v => this._convertLabels(v));
-    this.chart.data.datasets[0].data = this.myData;
-    this.chart.data.datasets[1].data = this.averageData;
-    this.chart.update();
+    setTimeout(() => {
+      this.chart.data.labels = this.labels.map(v => this._convertLabels(v));
+      this.chart.data.datasets[0].data = this.myData;
+      this.chart.data.datasets[1].data = this.averageData;
+      this.chart.update();
+    }, 0);
   }
 }
