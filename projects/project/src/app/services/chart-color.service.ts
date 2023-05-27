a926aa03296427663c2584f8f97699dd5b320ccb
kmahn <ahnmo777@gmail.com>
2023-05-27 20:15:16 +0900

    Update

--- projects/project/src/app/services/chart-color.service.ts
+++ projects/project/src/app/services/chart-color.service.ts
@@ -5,9 +5,13 @@ import { Injectable } from '@angular/core';
 })
 export class ChartColorService {
   getColors(length: number, opacity = 0.55): string[] {
-    const random = () => Math.floor(Math.random() * 225 + 30);
+    const random = () => Math.floor(Math.random() * 244 + 11);
+
     return Array.from({ length }).map(_ => {
-      const rgb = [random(), random(), random()];
+      let rgb = [random(), random(), random()];
+      while (rgb.reduce((a, b) => a + b, 0) > 550 || rgb.reduce((a, b) => a + b, 0) < 545) {
+        rgb = [random(), random(), random()];
+      }
       return `rgba(${rgb.join(', ')}, ${opacity}`;
     });
   }
